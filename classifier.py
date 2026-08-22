3c06998dcc015c1edb3373813d866e2d390a6af1
Desplanches Manuel <131704431+ManDes71@users.noreply.github.com>
2026-08-22 21:29:38 +0200

    Update

--- classifier.py
+++ classifier.py
@@ -44,8 +44,8 @@ nltk.download("punkt_tab", quiet=True)
 DB_HOST     = os.getenv("DB_HOST",     "localhost")
 DB_PORT     = int(os.getenv("DB_PORT", "5432"))
 DB_NAME     = os.getenv("DB_NAME",     "fluxrss")
-DB_USER     = os.getenv("DB_USER",     "manu")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "liLa@24")
+DB_USER     = os.getenv("DB_USER",     "user")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "mdp")
 
 TODAY = date.today()
 NBOCC_MIN = 2
