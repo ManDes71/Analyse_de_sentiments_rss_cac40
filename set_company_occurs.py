c17744173e356fb1867ebffabc062957acbb2d85
Desplanches Manuel <131704431+ManDes71@users.noreply.github.com>
2026-08-22 21:31:13 +0200

    Update

--- set_company_occurs.py
+++ set_company_occurs.py
@@ -24,8 +24,8 @@ from psycopg2.extras import RealDictCursor
 DB_HOST     = os.getenv("DB_HOST",     "localhost")
 DB_PORT     = int(os.getenv("DB_PORT", "5432"))
 DB_NAME     = os.getenv("DB_NAME",     "REMOVED")
-DB_USER     = os.getenv("DB_USER",     "REMOVED")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "REMOVED")
+DB_USER     = os.getenv("DB_USER",     "user")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "mdp")
 
 TODAY = date.today()
 
